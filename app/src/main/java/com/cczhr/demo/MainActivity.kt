a9e204392a2053c23a6d49b02eba546dda45d566
cczhr <464591548@qq.com>
2021-05-10 09:24:54 +0800

    Initial commit

---
+++ app/src/main/java/com/cczhr/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cczhr.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Android Gradle插件3.6.0及
+    }
+}
---
+++ lib/src/main/java/com/cczhr/lib/LIB.kt
@@ -0,0 +1,12 @@
+package com.cczhr.lib
+
+/**
+ * @author cczhr
+ * @description
+ * @since 2021/5/10
+ */
+class LIB {
+    companion object{
+        const val TEST=1
+    }
+}
